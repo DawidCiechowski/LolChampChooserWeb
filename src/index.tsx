6e0e32197bb596ff7466048ffabec03506c988ae
Dawid Ciechowski <dawid.ciechowski@gmail.com>
2024-06-01 02:14:08 +0200

    Homepage addded

--- src/index.tsx
+++ src/index.tsx
@@ -4,18 +4,12 @@ import './index.css';
 import App from './App';
 import { RouterProvider, createBrowserRouter } from 'react-router-dom';
 
-const router = createBrowserRouter([
-  {
-    path: "/",
-    element: <App />
-  }
-])
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <RouterProvider router={router} />
+    <App />
   </React.StrictMode>
 );
